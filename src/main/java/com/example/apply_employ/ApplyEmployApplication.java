c161f7e471d181aec4b6c881c4969287910e0c56
wjd514 <wjd514@naver.com>
2023-10-15 11:44:10 +0900

    Initial commit

---
+++ src/main/java/com/example/apply_employ/ApplyEmployApplication.java
@@ -0,0 +1,13 @@
+package com.example.apply_employ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplyEmployApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplyEmployApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apply_employ/ApplyEmployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apply_employ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplyEmployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
